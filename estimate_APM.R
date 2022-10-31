4018d5eeb0cc7abc23889c3d69cdfc4effbebe97
Hu Die <59279311+hudie-lab@users.noreply.github.com>
2022-10-31 09:44:27 +0800

    Update estimate_APM.R

--- estimate_APM.R
+++ estimate_APM.R
@@ -68,3 +68,53 @@ repcgc = solve.QP(Dmat = XTX, dvec=XTY, Amat = diag(1,3), bvec = rep(0, 3), meq=
 sum_XTX <- matrix(rowSums(apply(X_matrix,1,function(X) X%*%t(X))),nrow=3,byrow = F)
 inver_sum_XTX <- solve(sum_XTX)
 write.table(c(repcgc,sqrt(diag(inver_sum_XTX ))),paste0(dir,"_R-PCGC_asymptotic.txt"),col.names = c("esti_A","esti_P","esti_M","sd_A","sd_P","sd_M"))
+                                
+                                
+## to run analysis in parallel for 100 times
+library(quadprog)
+
+# input the directory of the GRM and the phenotype files and the prevalence K
+N <- 3000
+K <- 0.5
+simname <- paste0("bin_", K)
+dir <- "./"
+#Cc_generate(dir,1, hsqA=0.4,hsqP = 0.05,hsqM = 0.1, K=K,nsnps = 5000, nobs=N)
+
+sapply( 1:100, Cc_generate, dir=dir, hsqA=0.4,hsqP = 0.05,hsqM = 0.1, K=K,nsnps = 5000, nobs=N)
+
+# estima_CC(dir1=dir, times=24, nobs=N,K=K)
+sapply(1:100, estima_CC, dir1=dir, nobs=N, K=K)
+
+## to read and restore the data
+reml_result <- pcgc_result <- rpcgc_result <- matrix(nrow=108, ncol=6)
+for(times in 1:100){
+  varGCTA <- read.table(paste0(dir, simname,"_APM_", times, ".hsq"), header = F, sep = " ")[[1]]
+  
+  reml_result[times, 1:2] <- as.numeric(unlist(strsplit(varGCTA[(grepl("V.(G1.)/Vp_L", varGCTA))], "\t"))[2:3])
+  reml_result[times, 3:4] <- as.numeric(unlist(strsplit(varGCTA[(grepl("V.(G2.)/Vp_L", varGCTA))], "\t"))[2:3])
+  reml_result[times, 5:6] <- as.numeric(unlist(strsplit(varGCTA[(grepl("V.(G3.)/Vp_L", varGCTA))], "\t"))[2:3])
+  
+  varPCGC <- read.table(paste0(dir, simname, "_PCGC_", times, ".txt"), header = F, sep = ",")
+  
+  pcgc_result[times, 1:2] <- as.numeric(unlist(strsplit(varPCGC[9,], " "))[
+    nchar(unlist(strsplit(varPCGC[9,], " "))) > 0 ][2:3])
+  pcgc_result[times, 3:4] <- as.numeric(unlist(strsplit(varPCGC[10,], " "))[
+    nchar(unlist(strsplit(varPCGC[10,], " "))) > 0 ][2:3])
+  pcgc_result[times, 5:6] <- as.numeric(unlist(strsplit(varPCGC[11,], " "))[
+    nchar(unlist(strsplit(varPCGC[11,], " "))) > 0 ][2:3])
+  
+  varRpcgc <- as.numeric(unlist(
+    read.table(paste0(dir, simname, "_R-PCGC_", times, ".txt"), header = F, sep = ",")[,-1])[-1])
+  
+  rpcgc_result[times, 1:2] <- varRpcgc[1:2]
+  rpcgc_result[times, 3:4] <- varRpcgc[3:4]
+  rpcgc_result[times, 5:6] <- varRpcgc[5:6]
+}
+
+reml_result <- reml_result[-which(rowSums(is.na(reml_result))!=0),]
+pcgc_result <- pcgc_result[-which(rowSums(is.na(pcgc_result))!=0),]
+rpcgc_result <- rpcgc_result[-which(rowSums(is.na(rpcgc_result))!=0),]
+
+write.csv(reml_result, "./reml_result.csv")
+write.csv(pcgc_result, "./pcgc_result.csv")
+write.csv(rpcgc_result, "./rpcgc_result.csv")
